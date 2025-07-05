af7b0d3159d3524c99a0c36021dd1b6544f48250
anar_ceferov <anarceferov1996@gmail.com>
2025-07-05 15:49:17 +0400

    Auth fixed

---
+++ src/main/java/com/jafarov/quiz/admin/service/CostumUserDetailService.java
@@ -0,0 +1,32 @@
+package com.jafarov.quiz.admin.service;
+
+import com.jafarov.quiz.admin.entity.User;
+import com.jafarov.quiz.admin.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CostumUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CostumUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities("USER")
+                .accountLocked(!user.getStatus())
+                .build();
+    }
+}
