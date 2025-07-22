a22bc5c2946ae21015e5de4724c6105901b12825
anar_ceferov <anarceferov1996@gmail.com>
2025-07-22 23:57:05 +0400

    Question Edit Fixed

--- src/main/java/com/jafarov/quiz/admin/repository/QuestionRepository.java
+++ src/main/java/com/jafarov/quiz/admin/repository/QuestionRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -24,4 +25,8 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
     @EntityGraph(attributePaths={"answers"})
     @Query("SELECT q FROM Question q WHERE q.id = :id")
     Question getQuestionWithAnswersById(@Param("id") Long id);
+
+    @Modifying
+    @Query("UPDATE Question q SET q.isActive = :status WHERE q.id = :id")
+    void changeStatus(@Param("id") Long id, @Param("status") Boolean status);
 }
