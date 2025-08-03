5b4a493cc867a61afad77d4bfe1f173fb590ca47
anar_ceferov <anarceferov1996@gmail.com>
2025-08-03 16:22:00 +0400

    auth fixed

--- src/main/java/com/jafarov/quiz/service/CustomUserDetailsService.java
+++ src/main/java/com/jafarov/quiz/service/CustomUserDetailsService.java
@@ -1,20 +1,12 @@
 package com.jafarov.quiz.service;
 
-import com.jafarov.quiz.controller.CustomUserDetails;
+import com.jafarov.quiz.config.auth.CustomUserDetails;
 import com.jafarov.quiz.entity.User;
 import com.jafarov.quiz.repository.UserRepository;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Service;
-
-import java.io.IOException;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
